2039284b88f1059cfe08b6c9761dfe53874a16be
lipaz dahan <lipazdahan@gmail.com>
2020-03-15 16:55:43 +0200

    testcommmit

--- src/Component/TeacherComponents/Alerts/CCOneAlert.jsx
+++ src/Component/TeacherComponents/Alerts/CCOneAlert.jsx
@@ -25,7 +25,7 @@ export default class CCOneAlert extends Component {
                </div>
                <div className="row col-8 detailsOneAlert">
                    <div className="col-12">22/10/2020 שעה 12:00</div>
-                   <div className="col-12">רינת סיימה את אתגר 4 בהצלחה</div>
+                   <div className="col-12">עמית סיימה את אתגר 4 בהצלחה</div>
                </div>
            </div>
             
