eb7297543cd65350d4ca4af98dc338b1be5dc847
lipaz dahan <lipazdahan@gmail.com>
2020-03-15 19:15:13 +0200

    test2

--- src/Component/TeacherComponents/Alerts/CCOneAlert.jsx
+++ src/Component/TeacherComponents/Alerts/CCOneAlert.jsx
@@ -5,7 +5,7 @@ import { FaTrashAlt } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { MdMail } from  "react-icons/md";
 
-
+// test2
 
 export default class CCOneAlert extends Component {
     constructor(props){
