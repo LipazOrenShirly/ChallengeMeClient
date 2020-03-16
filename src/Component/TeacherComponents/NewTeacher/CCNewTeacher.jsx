d21ce34924e4785f28286ee10a610776a6c3f777
Oren Vardi <orenvardi1@gmail.com>
2020-03-16 17:03:59 +0200

    fetch POST new teacher

--- src/Component/TeacherComponents/NewTeacher/CCNewTeacher.jsx
+++ src/Component/TeacherComponents/NewTeacher/CCNewTeacher.jsx
@@ -3,52 +3,114 @@ import '../../../css/Style.css';
 import './styleNewTeacher.css'
 import Footer from '../../LittleComponents/Footer';
 import Logo from '../../LittleComponents/Logo'
+import localHost from '../../LittleComponents/LocalHost';
 
 export default class CCnewTeacher extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-    } 
-    
+        this.state = {
+            firstName: "",
+            lastName: "",
+            userName: "",
+            mail: "",
+            phone: "",
+            password: "",
+            school: ""
+        }
+        let local = true;
+        this.apiUrl = 'http://localhost:' + { localHost }.localHost + '/api/Teacher';
+        if (!local) {
+            this.apiUrl = 'http://proj.ruppin.ac.il/igroup2/??????'; //להשלים!!
+        }
+    }
+
+    Submit = (event) => {
+        console.log('state=' + this.state);
+
+        var data = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            userName: this.state.userName,
+            mail: this.state.mail,
+            phone: this.state.phone,
+            password: this.state.password,
+            school: this.state.school
+        }
+        console.log('data=' + data);
+        fetch(this.apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-type': 'application/json; charset=UTF-8'
+            })
+        })
+            .then(res => {
+                console.log('res=', res);
+                return res.json()
+            })
+            .then(
+                (result) => {
+                    console.log("fetch POST= ", result);
+                },
+                (error) => {
+                    console.log("err post=", error);
+                })
+            .then(
+                this.props.history.push({
+                    pathname: '/TeacherLogin',
+                })
+            );
+
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="container-fluid">
                 <div className="loginDiv">
-                <Logo></Logo>
-                <form>
-              <div className="form-group col-12">
-                <input type="text" className="form-control inputNewTeacher" id="NewTFirstName"  placeholder="שם פרטי" pattern="[א-ת]+" required></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="text" className="form-control inputNewTeacher" id="NewTLastName"  placeholder="שם משפחה" pattern="[א-ת]+" required></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="text" className="form-control inputNewTeacher" id="NewTUserName"  placeholder="שם משתמש" required></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="mail" className="form-control inputNewTeacher" id="NewTMail"  placeholder="כתובת מייל"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="phone" className="form-control inputNewTeacher" id="NewTPhone"  placeholder="פלאפון" pattern="[0][5][0-9]{8}$" required></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="password" className="form-control inputNewTeacher" id="NewTPassword"  placeholder="הזן ססמה"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required></input>
-            </div>
-     {/* Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters   */}
-            <div className="form-group col-12">
-                <input type="password" className="form-control inputNewTeacher" id="NewTPassword2"  placeholder="הזן ססמה בשנית"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="text" className="form-control inputNewTeacher" id="NewTSchool"  placeholder="מוסד לימודים שהינך מלמד בו" required></input>
-            </div>
-            <div className="col-12">
-            <button type="submit" id="submit" className="btn btn-info btnYellow">כניסה</button>
-            </div>
-            </form>
-            </div>
-            <Footer></Footer>
+                    <Logo></Logo>
+                    <form onSubmit={this.Submit}>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputNewTeacher" id="NewTFirstName" placeholder="שם פרטי" pattern="[א-ת]+" required
+                                onChange={(e) => this.setState({ firstName: e.target.value })} />
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputNewTeacher" id="NewTLastName" placeholder="שם משפחה" pattern="[א-ת]+" required
+                                onChange={(e) => this.setState({ lastName: e.target.value })} />
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputNewTeacher" id="NewTUserName" placeholder="שם משתמש" required
+                                onChange={(e) => this.setState({ userName: e.target.value })} />
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="mail" className="form-control inputNewTeacher" id="NewTMail" placeholder="כתובת מייל" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required
+                                onChange={(e) => this.setState({ mail: e.target.value })} />
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="phone" className="form-control inputNewTeacher" id="NewTPhone" placeholder="פלאפון" pattern="[0][5][0-9]{8}$" required
+                                onChange={(e) => this.setState({ phone: e.target.value })} />
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="password" className="form-control inputNewTeacher" id="NewTPassword" placeholder="הזן ססמה" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required
+                                onChange={(e) => this.setState({ password: e.target.value })} />
+                        </div>
+                        {/* Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters   */}
+                        <div className="form-group col-12">
+                            <input type="password" className="form-control inputNewTeacher" id="NewTPassword2" placeholder="הזן ססמה בשנית" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputNewTeacher" id="NewTSchool" placeholder="מוסד לימודים שהינך מלמד בו" required
+                                onChange={(e) => this.setState({ school: e.target.value })} />
+                        </div>
+                        <div className="col-12">
+                            <button type="submit" id="submit" className="btn btn-info btnYellow">כניסה</button>
+                        </div>
+                    </form>
+                </div>
+                <Footer></Footer>
 
 
-              
+
             </div>
 
         );
