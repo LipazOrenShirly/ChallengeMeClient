31dbdfd55badef900c6ee1a6f05728c050a87077
Oren Vardi <orenvardi1@gmail.com>
2020-03-16 16:12:20 +0200

    remember me + fetch GET

--- src/Component/TeacherComponents/TeacherLogin/CCTeacherLogin.jsx
+++ src/Component/TeacherComponents/TeacherLogin/CCTeacherLogin.jsx
@@ -1,36 +1,82 @@
 import React, { Component } from 'react';
 import '../../../css/Style.css';
 import './styleTeacherLogin.css'
-
-
+import localHost from '../../LittleComponents/LocalHost';
 
 export default class CCTeacherLogin extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      username: "",
+      password: "",
+      remember: false,
+      teachersFromDB: []
+    };
+    let local = true;
+    this.apiUrl = 'http://localhost:' + {localHost}.localHost + '/api/Teacher';
+    if (!local) {
+      this.apiUrl = 'http://proj.ruppin.ac.il/igroup2/??????'; //להשלים!!
+    }
   }
-
 
   NewTeacher = () => {
     this.props.history.push({
       pathname: '/newTeacher',
     })
   }
+
   ForgetPassword = () => {
     this.props.history.push({
       pathname: '/TeacherForgetPassword',
     })
   }
+
   Submit = () => {
-    this.props.history.push({
-      pathname: '/HomePageTeacher',
-    });
-    // sessionStorage.setItem('usename', 'value');
-    // sessionStorage.setItem('password', 'value');
+    if(this.state.remember === true) {
+      localStorage.setItem('username', this.state.username);
+      localStorage.setItem('password', this.state.password);
+    }
+
+    // this.props.history.push({
+    //   pathname: '/HomePageTeacher',
+    // });
+
+    var data ={
+      username: sessionStorage.getItem('username'),
+      password: sessionStorage.getItem('password'),
+    };
+
+    fetch(this.apiUrl//+'?username='+data.username+'&password='+data.password 
+    ,{
+      method: 'GET',
+      // mode: 'no-cors',
+      headers: new Headers({
+        'Content-Type': 'application/json; charset=UTF-8',
+      })
+    })
+      .then(res => {
+        console.log('res=', res);
+        console.log('res.status', res.status);
+        console.log('res.ok', res.ok);
+        return res.json();
+      })
+      .then(
+        (result) => {
+          console.log("Submit= ", result[0].teacherID);
+          this.setState({teachersFromDB:result[0].teacherID})
+        },
+        (error) => {
+          console.log("err get=", error);
+        })
+      .then(
+        this.props.history.push({
+          pathname: '/HomePageTeacher/'+this.state.teachersFromDB,
+        })
+      );
   }
 
-
   render() {
+    {console.log('localHost='+{localHost}.localHost)}
     return (
       <div className="container-fluid">
         <div className="loginDiv">
@@ -40,16 +86,26 @@ export default class CCTeacherLogin extends Component {
           <form onSubmit={this.Submit}>
             <div className="form-group col-12">
               <input type="text" className="form-control inputRounded" id="unameId" placeholder="הכנס שם משתמש" required
-              onChange={(e)=>sessionStorage.setItem('username', e.target.value)}/>
+                value={localStorage.getItem('username')}
+                onChange={(e) => {
+                  sessionStorage.setItem('username', e.target.value);
+                  this.setState({username: e.target.value});
+                }} />
             </div>
             <div className="form-group col-12">
-              <input type="password" className="form-control inputRounded" id="apasswordId" placeholder="הכנס ססמה" required 
-              onChange={(e)=>sessionStorage.setItem('password', e.target.value)}/>
+              <input type="password" className="form-control inputRounded" id="apasswordId" placeholder="הכנס ססמה" required
+                value={localStorage.getItem('password')}
+                onChange={(e) => {
+                  sessionStorage.setItem('password', e.target.value);
+                  this.setState({password: e.target.value});
+                  }} />
             </div>
             <div className="rememberMeDiv">
               <label>
                 זכור אותי&nbsp;&nbsp;
-                <input type="checkbox" name="remember"></input>
+                <input type="checkbox" name="remember" onChange={(e) => {
+                  this.setState({remember: e.target.checked});
+                  }}/>
               </label>
             </div><br />
 
