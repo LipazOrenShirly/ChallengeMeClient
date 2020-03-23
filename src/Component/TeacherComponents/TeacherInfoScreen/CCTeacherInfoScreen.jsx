b3bc2cb05025dce38c6c9465771904a3cfd81511
Oren Vardi <challenge.me555555@gmail.com>
2020-03-23 21:11:52 +0200

    update Teacher Details fetch

--- src/Component/TeacherComponents/TeacherInfoScreen/CCTeacherInfoScreen.jsx
+++ src/Component/TeacherComponents/TeacherInfoScreen/CCTeacherInfoScreen.jsx
@@ -5,26 +5,97 @@ import Footer from '../../LittleComponents/Footer';
 import Logo from '../../LittleComponents/Logo';
 import $ from 'jquery';
 import NavBar from '../../LittleComponents/NavBar';
+import localHost from '../../LittleComponents/LocalHost';
 
 export default class CCTeacherInfoScreen extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-    } 
-    
-    updateDetails=()=>{
-        $('#UpdateTFirstName').prop("disabled", false);
-        $('#UpdateTLastName').prop("disabled", false);
-        $('#UpdateTUserName').prop("disabled", false);
-        $('#UpdateTMail').prop("disabled", false);
-        $('#UpdateTPhone').prop("disabled", false);
-        $('#UpdateTPassword').prop("disabled", false);
-        $('#UpdateTPassword2').prop("disabled", false);
-        $('#UpdateTSchool').prop("disabled", false);
-        $('#submit').hide();
-        $('.hideFirst').css("display","inline-block")
+        this.state = {
+            teacher: {}
+        }
+        let local = true;
+        this.apiUrl = 'http://localhost:' + { localHost }.localHost + '/api/Teacher';
+        if (!local) {
+            this.apiUrl = 'http://proj.ruppin.ac.il/igroup2/??????'; //להשלים!!
+        }
     }
-    btnClick=()=>{
-        if(window.confirm("אתה בטוח שאתה רוצה לבטל את השינויים?")) //נפתח לו חלון, אם לוחץ אישור זה מבטל ומרענן את הדף אם לוחץ לא לא קורה כלום
+
+    componentWillMount() {
+        console.log('teacherID = ');
+        fetch(this.apiUrl + '?teacherID=8'
+            , {
+                method: 'GET',
+                headers: new Headers({
+                    'Content-Type': 'application/json; charset=UTF-8',
+                })
+            })
+            .then(res => {
+                console.log('res=', res);
+                console.log('res.status', res.status);
+                console.log('res.ok', res.ok);
+                return res.json();
+            })
+            .then(
+                (result) => {
+                    console.log("teacher= ", result);
+                    this.setState({ teacher: result[0] });
+                    console.log(this.state.teacher);
+                },
+                (error) => {
+                    console.log("err get=", error);
+                })
+            .then(() => {
+                $('#UpdateTFirstName').val(this.state.teacher.firstName);
+                $('#UpdateTLastName').val(this.state.teacher.lastName);
+                $('#UpdateTUserName').val(this.state.teacher.userName);
+                $('#UpdateTMail').val(this.state.teacher.mail);
+                $('#UpdateTPhone').val(this.state.teacher.phone);
+                $('#UpdateTPassword').val(this.state.teacher.password);
+                $('#UpdateTPassword2').val(this.state.teacher.password);
+                $('#UpdateTSchool').val(this.state.teacher.school);
+            });
+    }
+
+    EnableFields = () => {
+        $('.inputUpdateTeacher').prop("disabled", false);
+        $('#submit').hide();
+        $('.hideFirst').css("display", "inline-block")
+    }
+
+    UpdateDetails = (event) => {
+        var updatedDetails = {
+            firstName: $('#UpdateTFirstName').val(),
+            lastName: $('#UpdateTLastName').val(),
+            userName: $('#UpdateTUserName').val(),
+            mail: $('#UpdateTMail').val(),
+            phone: $('#UpdateTPhone').val(),
+            password: $('#UpdateTPassword').val(),
+            school: $('#UpdateTSchool').val(),
+            teacherID: 8
+        }
+        fetch(this.apiUrl, {
+            method: 'PUT',
+            body: JSON.stringify(updatedDetails),
+            headers: new Headers({
+                'Content-type': 'application/json; charset=UTF-8'
+            })
+        })
+            .then(res => {
+                console.log('res=', res);
+                return res.json()
+            })
+            .then(
+                (result) => {
+                    console.log("fetch PUT= ", result);
+                },
+                (error) => {
+                    console.log("err PUT=", error);
+                });
+        event.preventDefault();
+    }
+
+    btnClick = () => {
+        if (window.confirm("אתה בטוח שאתה רוצה לבטל את השינויים?")) //נפתח לו חלון, אם לוחץ אישור זה מבטל ומרענן את הדף אם לוחץ לא לא קורה כלום
             window.location.reload();
     }
 
@@ -32,50 +103,45 @@ export default class CCTeacherInfoScreen extends Component {
         return (
             <div className="container-fluid">
                 <div className="loginDiv">
-                <NavBar></NavBar><br/>
-                <form onSubmit={(e) => {this.updateDetails(); e.preventDefault();}}>
-                <div className="myInformation">הפרטים שלי</div>
-              <div className="form-group col-12">
-                <input type="text" className="form-control inputUpdateTeacher" id="UpdateTFirstName"  placeholder="שם פרטי" defaultValue="גדעון" pattern="[א-ת]+" disabled></input>
+                    <NavBar></NavBar><br />
+                    <form onSubmit={this.UpdateDetails}>
+                        <div className="myInformation">הפרטים שלי</div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputUpdateTeacher" id="UpdateTFirstName" placeholder="שם פרטי" pattern="[א-ת]+" disabled></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputUpdateTeacher" id="UpdateTLastName" placeholder="שם משפחה" pattern="[א-ת]+" disabled></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputUpdateTeacher" id="UpdateTUserName" placeholder="שם משתמש" disabled></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="mail" className="form-control inputUpdateTeacher" id="UpdateTMail" placeholder="כתובת מייל" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" disabled></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="phone" className="form-control inputUpdateTeacher" id="UpdateTPhone" placeholder="פלאפון" pattern="[0][5][0-9]{8}$" disabled></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="password" className="form-control inputUpdateTeacher" id="UpdateTPassword" placeholder="הזן ססמה" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" disabled></input>
+                        </div>
+                        {/* Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters   */}
+                        <div className="form-group col-12">
+                            <input type="password" className="form-control inputUpdateTeacher" id="UpdateTPassword2" placeholder="הזן ססמה בשנית" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" disabled></input>
+                        </div>
+                        <div className="form-group col-12">
+                            <input type="text" className="form-control inputUpdateTeacher" id="UpdateTSchool" placeholder="מוסד לימודים שהינך מלמד בו" disabled></input>
+                        </div>
+                        <div className="col-12">
+                            <button type="button" id="submit" className="btn btn-info btnYellow" onClick={this.EnableFields} >עדכן פרטים</button>
+                        </div>
+                        <div className="col-12 hideFirst" >
+                            <button type="button" id="cancel" className="btn btn-info col-4 btnYellow cencelBtn" onClick={this.btnClick} >בטל</button>
+                            <button type="submit" id="save" className="btn btn-info col-8 btnYellow"  >שמור</button>
+                        </div>
+                    </form>
+                </div>
+                <Footer></Footer>
             </div>
-            <div className="form-group col-12">
-                <input type="text" className="form-control inputUpdateTeacher" id="UpdateTLastName" placeholder="שם משפחה" defaultValue="כהן" pattern="[א-ת]+" disabled></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="text" className="form-control inputUpdateTeacher" id="UpdateTUserName" placeholder="שם משתמש" defaultValue="גדעון123" disabled></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="mail" className="form-control inputUpdateTeacher" id="UpdateTMail"  placeholder="כתובת מייל" defaultValue="gidon@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" disabled></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="phone" className="form-control inputUpdateTeacher" id="UpdateTPhone" placeholder="פלאפון"  defaultValue="0545588756" pattern="[0][5][0-9]{8}$" disabled></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="password" className="form-control inputUpdateTeacher" id="UpdateTPassword" placeholder="הזן ססמה"  defaultValue="123456"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" disabled></input>
-            </div>
-     {/* Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters   */}
-            <div className="form-group col-12">
-                <input type="password" className="form-control inputUpdateTeacher" id="UpdateTPassword2"   placeholder="הזן ססמה בשנית" defaultValue="123456"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" disabled></input>
-            </div>
-            <div className="form-group col-12">
-                <input type="text" className="form-control inputUpdateTeacher" id="UpdateTSchool"  placeholder="מוסד לימודים שהינך מלמד בו" defaultValue="בית ספר רימון" disabled></input>
-            </div>
-            <div className="col-12">
-            <button type="submit" id="submit" className="btn btn-info btnYellow"  >עדכן פרטים</button>
-            </div>
-            <div className="col-12 hideFirst" >
-            <button type="button" id="cancel" className="btn btn-info col-4 btnYellow cencelBtn" onClick={this.btnClick} >בטל</button>
-            <button type="button" id="save" className="btn btn-info col-8 btnYellow"  >שמור</button>
-            </div>
-
-            </form>
-            </div>
-            <Footer></Footer>
-
-
-              
-            </div>
-
         );
     };
 }
