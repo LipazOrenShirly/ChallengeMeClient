19a186e7f70fa89dfa9b0a53d9f7fec1e37361a6
Oren Vardi <challenge.me555555@gmail.com>
2020-03-23 19:16:44 +0200

    delete student

--- src/Component/TeacherComponents/HomePageTeacher/CCOneStudent.jsx
+++ src/Component/TeacherComponents/HomePageTeacher/CCOneStudent.jsx
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 import '../../../css/Style.css';
 import './styleHomePageTeacher.css';
 import CCStudents from './CCStudents';
-
+import localHost from '../../LittleComponents/LocalHost';
+import { FaTrashAlt } from "react-icons/fa";
 
 export default class CCOneStudent extends Component {
     constructor(props) {
         super(props);
-
+        let local = true;
+        this.apiUrl = 'http://localhost:' + { localHost }.localHost + '/api/Student';
+        if (!local) {
+            this.apiUrl = 'http://proj.ruppin.ac.il/igroup2/??????'; //להשלים!!
+        }
     }
 
+    DeleteStudent = () => {
+        fetch(this.apiUrl + '?studentID=' + this.props.student.studentID, {
+            method: 'DELETE',
+            headers: new Headers({
+                'accept': 'application/json; charset=UTF-8' 
+            })
+        })
+            .then(res => {
+                console.log('res=', res);
+                return res.json()
+            })
+            .then(
+                (result) => {
+                    console.log("fetch DELETE= ", result);
+                },
+                (error) => {
+                    console.log("err post=", error);
+                })
+                .then(
+                    window.location.reload());
+    }
 
     render() {
         return (
 
             <div className="classNameHome col-2" >
+                <div className="iconDiv" onClick={this.DeleteStudent}><FaTrashAlt /></div>
                 <span className="verticalMiddle">{this.props.student.firstName}</span>
             </div>
         );
