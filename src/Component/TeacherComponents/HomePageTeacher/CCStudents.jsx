65bb6625a9ae20de7f605e86e34d53751910696d
Oren Vardi <challenge.me555555@gmail.com>
2020-03-23 19:16:14 +0200

    edit class name

--- src/Component/TeacherComponents/HomePageTeacher/CCStudents.jsx
+++ src/Component/TeacherComponents/HomePageTeacher/CCStudents.jsx
@@ -7,7 +7,10 @@ import NavBar from '../../LittleComponents/NavBar';
 import CCOneStudent from './CCOneStudent';
 import SearchBarHomeTeacher from '../../LittleComponents/SearchBarHomeTeacher';
 import localHost from '../../LittleComponents/LocalHost';
+import { MdCreate } from "react-icons/md";
+import { IoMdCheckmark } from "react-icons/io";
 
+import $ from 'jquery';
 
 export default class CCStudents extends Component {
     constructor(props) {
@@ -22,11 +25,11 @@ export default class CCStudents extends Component {
             this.apiUrl = 'http://proj.ruppin.ac.il/igroup2/??????'; //להשלים!!
         }
     }
-
-    componentDidMount = () => {
-        //get by: 
-        
-        fetch(this.apiUrl+'?classID='+this.state.Class.classID
+    
+     componentDidMount = () => {
+        $('#BTNsaveClassName').hide();
+        $('#className').val(this.state.Class.className);
+        fetch(this.apiUrl + '?classID=' + this.state.Class.classID
             , {
                 method: 'GET',
                 headers: new Headers({
@@ -49,18 +52,64 @@ export default class CCStudents extends Component {
                 });
     }
 
-    render() {
+    EditClassName = () => { 
+        $('#className').prop("disabled", false);
+        $('#BTNeditClassName').hide();
+        $('#BTNsaveClassName').show();
+    }
 
+    UpdateClassName = () => {
+        var updatedClass = {
+            className: $('#className').val(),
+            classID: this.state.Class.classID,
+            teacherID: this.state.Class.teacherID
+        }
+
+        var apiUrlClass = 'http://localhost:' + { localHost }.localHost + '/api/Class'
+        fetch(apiUrlClass, {
+            method: 'PUT',
+            body: JSON.stringify(updatedClass),
+            headers: new Headers({
+              'Content-type': 'application/json; charset=UTF-8' 
+            })
+          })
+            .then(res => {
+              console.log('res=', res);
+              return res.json()
+            })
+            .then(
+              (result) => {
+                console.log("fetch PUT= ", result);
+              },
+              (error) => {
+                console.log("err post=", error);
+              });
+              $('#BTNsaveClassName').hide();
+              $('#BTNeditClassName').show();
+              $('#className').prop("disabled", true);
+
+
+
+
+    }
+
+    render() {
         return (
             <div className="container-fluid">
                 <NavBar></NavBar><br /><br />
                 <SearchBarHomeTeacher />
                 <div className="turkiz">
-                    {this.state.Class.className}</div>
+                    <input type="text" className = "classInput" id = "className" disabled ></input>
+                </div>
+                <div className="iconDiv" id="BTNeditClassName" onClick={this.EditClassName}><MdCreate /></div>
+                <div className="iconDiv" id="BTNsaveClassName" visibility = 'hidden' onClick={this.UpdateClassName}><IoMdCheckmark /></div>
+                <br />
                 {
                     this.state.StudentArr.map((item) =>
                         <CCOneStudent teacherID={this.state.teacherID} student={item} SendDataToClasses={this.getDataFromOneClass} />
                     )}
+                <div className="AddnewStudent" id="AddnewStudent" onClick={this.AddStudent}>הוספת תלמיד +</div>
+
                 <Footer></Footer>
             </div>
         );
