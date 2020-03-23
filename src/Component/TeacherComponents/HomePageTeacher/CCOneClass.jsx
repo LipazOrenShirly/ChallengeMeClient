4f336799a38e40dc4eabeb18a755bd33f8ee30a8
Oren Vardi <challenge.me555555@gmail.com>
2020-03-23 19:16:29 +0200

    delete class

--- src/Component/TeacherComponents/HomePageTeacher/CCOneClass.jsx
+++ src/Component/TeacherComponents/HomePageTeacher/CCOneClass.jsx
@@ -3,12 +3,39 @@ import '../../../css/Style.css';
 import './styleHomePageTeacher.css';
 import CCStudents from './CCStudents';
 import localHost from '../../LittleComponents/LocalHost';
+import { FaTrashAlt } from "react-icons/fa";
 
 
 export default class CCOneClass extends Component {
     constructor(props) {
         super(props);
+        let local = true;
+        this.apiUrl = 'http://localhost:' + { localHost }.localHost + '/api/Class';
+        if (!local) {
+            this.apiUrl = 'http://proj.ruppin.ac.il/igroup2/??????'; //להשלים!!
+        }
+    }
 
+    DeleteClass = () => {
+        alert('delete class');
+        fetch(this.apiUrl + '?classID=' + this.props.class.classID, {
+            method: 'DELETE',
+            headers: new Headers({
+                'accept': 'application/json; charset=UTF-8'
+            })
+        })
+            .then(res => {
+                console.log('res=', res);
+                return res.json()
+            })
+            .then(
+                (result) => {
+                    console.log("fetch DELETE= ", result);
+                },
+                (error) => {
+                    console.log("err post=", error);
+                });
+        window.location.reload();
     }
 
     studentsPage = () => {
@@ -16,24 +43,10 @@ export default class CCOneClass extends Component {
     }
     render() {
         return (
-            <div className="classNameHome col-2" onClick={this.studentsPage}>
-                <span className="verticalMiddle">{this.props.class.className}</span>
+            <div className="classNameHome col-2" >
+                <div className="iconDiv" onClick={this.DeleteClass}><FaTrashAlt /></div>
+                <span className="verticalMiddle" onClick={this.studentsPage}>{this.props.class.className}</span>
             </div>
         );
     };
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
