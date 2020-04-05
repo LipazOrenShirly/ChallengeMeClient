70eadec500e30c1e4497573112855b9a9484943d
Oren Vardi <orenvardi1@gmail.com>
2020-04-05 13:51:10 +0300

    updates on teacher login

--- src/Component/TeacherComponents/TeacherLogin/CCTeacherLogin.jsx
+++ src/Component/TeacherComponents/TeacherLogin/CCTeacherLogin.jsx
@@ -21,9 +21,10 @@ export default class CCTeacherLogin extends Component {
       newPassword2: "",
       HasUserNameValError: true,
       HasPasswordError: true,
-      showChangePassword: false
+      HasnewPassword1Error: true,
+      HasnewPassword2Error: true,
+      showChangePassword: false,
     };
-
     let local = true;
     this.apiUrl = 'http://localhost:' + { localHost }.localHost + '/api/Teacher';
     if (!local) {
@@ -42,7 +43,6 @@ export default class CCTeacherLogin extends Component {
     if (this.state.Password != null)
       this.setState({ HasPasswordError: false });
     // $('#apasswordId').val(localStorage.getItem('password') != null ? localStorage.getItem('password') + '' : "");
-
   }
 
   NewTeacher = () => {
@@ -59,25 +59,63 @@ export default class CCTeacherLogin extends Component {
     })
   }
 
+  saveCredentials = (Username, Password) => {
+    // שמירה של השם משתמש והסיסמה בסשן סטורג' כדי שתהיה בדיקה של פרטי החיבור בטעינה של כל עמוד בהמשך
+    sessionStorage.setItem('password', Password);
+    sessionStorage.setItem('username', Username);
+
+    //אם מסומן זכור אותי שומר בלוקל סטורז
+    if ($('#remember').prop('checked') === true) {
+      localStorage.setItem('username', Username);
+      localStorage.setItem('password', Password);
+    }
+  }
+
   static contextType = ProjectContext;
+
+  SubmitNewPassword = (event) => {
+    this.setState({ showChangePassword: false });
+    const { Username, newPassword1 } = this.state;
+    this.saveCredentials(Username, newPassword1);
+    const user = this.context;
+    fetch(this.apiUrl + '?teacherID=' + user.teacherID + '&password=' + this.state.newPassword1
+      , {
+        method: 'PUT',
+        headers: new Headers({
+          'Content-Type': 'application/json; charset=UTF-8',
+        })
+      })
+      .then(res => {
+        console.log('res=', res);
+        console.log('res.status', res.status);
+        console.log('res.ok', res.ok);
+        return res.json();
+      })
+      .then(
+        (result) => {
+          console.log("Submit= ", result);
+          console.log("Submit= ", JSON.stringify(result));
+          Swal.fire({
+            title: 'מעולה!',
+            text: 'הסיסמה שונתה בהצלחה',
+            icon: 'success',
+            confirmButtonColor: '#e0819a',
+          })
+            .then(
+              this.props.history.push('/HomePageTeacher/'));
+        },
+        (error) => {
+          console.log("err get=", error);
+        });
+    event.preventDefault();
+  }
 
   Submit = (event) => {
     const user = this.context;
     if (!this.state.HasUserNameValError && !this.state.HasPasswordError) //אם אין הערות
     {
       const { Username, Password } = this.state;
-
-
-      // שמירה של השם משתמש והסיסמה בסשן סטורג' כדי שתהיה בדיקה של פרטי החיבור בטעינה של כל עמוד בהמשך
-      sessionStorage.setItem('password', Password);
-      sessionStorage.setItem('username', Username);
-
-      //אם מסומן זכור אותי שומר בלוקל סטורז
-      if ($('#remember').prop('checked') === true) {
-        localStorage.setItem('username', Username);
-        localStorage.setItem('password', Password);
-      }
-
+      this.saveCredentials(Username, Password);
 
       //בעזרת גט בודק אם השם משתמש וססמה קיימים במערכת
       fetch(this.apiUrl + '?username=' + Username + '&password=' + Password
@@ -98,44 +136,11 @@ export default class CCTeacherLogin extends Component {
             console.log("Submit= ", result);
             console.log("Submit= ", JSON.stringify(result));
             if (result.TeacherID != 0) { //אם המורה קיים בדאטה בייס
-              user.setTeacher(result.TeacherID); //אם קיים שמור בקונטקט
-
-
+              user.setTeacher(result.TeacherID); //אם קיים אז תשמור בקונטקט
               if (result.TempPassword == 0)//אם לא סיסמה זמנית תעבור לעמוד הבא
                 this.props.history.push('/HomePageTeacher/');
-
               else {//אם זה סיסמה זמנית אז
-
-                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                // this.setState({ showChangePassword: true });
-                const { value: formValues } = Swal.fire({
-                  title: 'הכנס ססמה חדשה',
-                  html:
-                    `<input type="password" placeholder="הזן ססמה חדשה" id="swal-input1"  class="swal2-input inputRounded">
-                    <input type="password" placeholder="הזן ססמה שנית" id="swal-input2" class="swal2-input inputRounded">`,
-                  focusConfirm: false,
-                  showCloseButton: true,
-                  showCancelButton: true,
-                  cancelButtonText:
-                    '<i class="fa fa-thumbs-down">בטל</i>',
-                  cancelButtonAriaLabel: 'Thumbs down',
-                  confirmButtonText:
-                    '<i class="fa fa-thumbs-up"></i> אשר',
-                  confirmButtonAriaLabel: 'Thumbs up, great!',
-
-                  preConfirm: () => {
-                    return [
-                      document.getElementById('swal-input1').value,
-                      document.getElementById('swal-input2').value
-                    ]
-
-                  }
-                })
-
-                if (formValues) {
-                  Swal.fire(JSON.stringify(formValues))
-                }
-
+                this.setState({ showChangePassword: true });
               }
             }
             else {
@@ -149,6 +154,7 @@ export default class CCTeacherLogin extends Component {
     }
     event.preventDefault();
   }
+
   onRecieveNewPassword = () => {
     //take the password and change the db
     let p1 = $('#swal-input1').val();
@@ -156,6 +162,7 @@ export default class CCTeacherLogin extends Component {
     alert("new password is: " + p1);
     this.setState({ showChangePassword: false });
   }
+
   render() {
     const {
       Username,
@@ -167,11 +174,10 @@ export default class CCTeacherLogin extends Component {
     return (
       <div className="container-fluid">
         <div className="loginDiv">
-
           <div className="col-12">
             <img className="logoImgLoginTeacher" src={require('../../../img/logoChallengeMe.svg')} />
           </div>
-          <form onSubmit={this.Submit}>
+          <form onSubmit={this.state.showChangePassword ? this.SubmitNewPassword : this.Submit}>
             <div className="form-group col-12">
               <Textbox  // כדי שיפעלו הולידציות שמים את האינפוט בטקסט בוקס
                 attributesInput={{
@@ -217,7 +223,7 @@ export default class CCTeacherLogin extends Component {
                 validationOption={{
                   check: true, // Optional.[Bool].Default: true. To determin if you need to validate.
                   required: true, // Optional.[Bool].Default: true. To determin if it is a required field.
-                  customFunc: pas => { //Minimum eight characters, at least one letter, one number and one special character:                                            :
+                  customFunc: pas => { //Minimum eight characters, at least one letter, one number and                      :
                     const reg = /^(?=.*[A-Za-z])(?=.*\d)([@$!%*#?&]*)[A-Za-z\d@$!%*#?&]{8,}$/;
                     if (reg.test(pas)) {
                       return true;
@@ -230,6 +236,82 @@ export default class CCTeacherLogin extends Component {
               />
             </div>
 
+            {/* אם הסיסמה זמנית מראה את 2 השדות לכתיבת סיסמה חדשה */}
+            {this.state.showChangePassword &&
+              <div>
+                <div className="form-group col-12">הסיסמה שלך זמנית, עליך לבחור סיסמה חדשה</div>
+                <div className="form-group col-12">
+                  <Textbox  // כדי שיפעלו הולידציות שמים את האינפוט בטקסט בוקס
+                    attributesInput={{
+                      id: 'newPassword1',
+                      type: 'password',
+                      placeholder: 'הזן ססמה חדשה',
+                      className: "form-control inputRounded"
+                    }}
+                    value={newPassword1}
+                    validationCallback={res =>
+                      this.setState({ HasnewPassword1Error: res, validate: false })
+                    }
+                    onChange={(newPassword1, e) => { //כל שינוי הוא שומר בסטייט
+                      this.setState({ newPassword1 });
+                      console.log(e);
+                    }}
+                    onBlur={(e) => { console.log(e) }} // Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
+                    validationOption={{
+                      check: true, // Optional.[Bool].Default: true. To determin if you need to validate.
+                      required: true, // Optional.[Bool].Default: true. To determin if it is a required field.
+                      customFunc: pas => { //Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+                        const reg = /^(?=.*[A-Za-z])(?=.*\d)([@$!%*#?&]*)[A-Za-z\d@$!%*#?&]{8,}$/;
+                        if (reg.test(pas)) {
+                          return true;
+                        } else {
+                          this.setState({ HasnewPassword1Error: true });
+                          return "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number";
+                        }
+                      }
+                    }}
+                  />
+                </div>
+                <div className="form-group col-12">
+                  <Textbox  // כדי שיפעלו הולידציות שמים את האינפוט בטקסט בוקס
+                    attributesInput={{
+                      id: 'newPassword2',
+                      type: 'password',
+                      placeholder: 'הזן ססמה בשנית',
+                      className: "form-control inputRounded"
+                    }}
+                    value={newPassword2}
+                    validationCallback={res =>
+                      this.setState({ HasnewPassword2Error: res, validate: false })
+                    }
+                    onChange={(newPassword2, e) => { //כל שינוי הוא שומר בסטייט
+                      this.setState({ newPassword2 });
+                      console.log(e);
+                    }}
+                    onBlur={(e) => { console.log(e) }} // Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
+                    validationOption={{
+                      check: true, // Optional.[Bool].Default: true. To determin if you need to validate.
+                      required: true, // Optional.[Bool].Default: true. To determin if it is a required field.
+                      customFunc: pas => { //Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+                        const reg = /^(?=.*[A-Za-z])(?=.*\d)([@$!%*#?&]*)[A-Za-z\d@$!%*#?&]{8,}$/;
+                        if (reg.test(pas)) {
+                          if (newPassword2 == newPassword1)
+                            return true;
+                          else {
+                            this.setState({ HasnewPassword2Error: true });
+                            return "not like first password";
+                          }
+                        } else {
+                          this.setState({ HasnewPassword2Error: true });
+                          return "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number";
+                        }
+                      }
+                    }}
+                  />
+                </div>
+              </div>
+            }
+
             <div className="rememberMeDiv">
               <label>
                 זכור אותי&nbsp;&nbsp;
@@ -241,38 +323,6 @@ export default class CCTeacherLogin extends Component {
               <div id="errorFromServer" className="react-inputs-validation__error___2aXSp"></div>
             </div>
             <h5 onClick={this.ForgetPassword}> שכחתי סיסמה</h5>
-            {/* <SweetAlert
-        show={this.state.showChangePassword}
-        title="אנא הכנס ססמה חדשה"
-        text="הכנס ססמה חדשה כאן"
-        input="text"
-        //  html={`<div>
-        //  <div className="form-group col-12">
-        //                     <input type="text" className="form-control inputRounded" id="NewTLastName" placeholder="שם משפחה" pattern="[א-ת]+" required
-        //                      />
-        //                 </div>
-        //                 <div className="form-group col-12">
-        //                     <input type="text" className="form-control inputRounded" id="NewTUserName" placeholder="שם משתמש" required
-        //                       />
-        //                 </div>
-        //  </div>`}
-
-        icon="success"
-        onConfirm={() => this.setState({ showChangePassword: false })}
-      /> */}
-            {/* <SweetAlert
-              input
-              inputType="password"
-              show={this.state.showChangePassword}
-              title="Enter Password"
-              
-              // required
-              // validationMsg="You must enter your password!"
-              onConfirm={this.onRecieveNewPassword}
-            >
-</SweetAlert> */}
-            {/* <div><input type="password" id="swal-input1" class="swal2-input"><input  type="password" id="swal-input2" class="swal2-input"></input></div> */}
-
           </form>
           <div onClick={this.NewTeacher}>
             <h6> משתמש חדש? הרשם כאן</h6>
