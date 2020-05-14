fc8a007a10a627ac05878f2524ab4eda410a316c
lipazd <lipazdahan@gmail.com>
2020-05-14 17:28:22 +0300

    דד

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ import Camera from './Component/StudentComponents/StudentHomePage/CCcamera';
 import ChooseAvatar from './Component/StudentComponents/chooseAvatar/CCChooseAvatar';
 import { waitForMassege} from './push-notification';
 import { ProjectProvider } from './Context/ProjectContext';
-import ReactNotification from 'react-notifications-component';
+// import ReactNotification from 'react-notifications-component';
 // import 'react-notifications-component/dist/theme.css';
  import { initializeFirebase } from './push-notification.js';
 
@@ -56,7 +56,7 @@ function App() {
 
   return (
     <div className="App">
-      <ReactNotification />
+      {/* <ReactNotification /> */}
 
       <ProjectProvider value={user}>
         <Switch>
